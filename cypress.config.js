211cab1dcbcb152a1283adc8f56adb0186ce1a44
BANDEC <roly96robledo@gmail.com>
2022-12-05 17:45:53 -0500

    commit video 1.1

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,5 @@
 const { defineConfig } = require("cypress");
 
-const _ = require('lodash')
-const del = require('del')
-
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
 
@@ -20,20 +17,6 @@ module.exports = defineConfig({
 
   e2e: {
     setupNodeEvents(on, config) {
-
-      on('after:spec', (spec, results) => {
-        if (results && results.video) {
-          // Do we have failures for any retry attempts?
-          const failures = _.some(results.tests, (test) => {
-            return _.some(test.attempts, { state: 'failed' })
-          })
-          if (!failures) {
-            // delete the video if the spec passed and no tests retried
-            return del(results.video)
-          }
-        }
-      })
-
       require('cypress-mochawesome-reporter/plugin')(on);
 
     },
